5d7561159f17b265c0972147cc48449a1596b661
Ben Allfree <ben@benallfree.com>
2013-07-30 19:45:51 +0000

    Updated config

--- wp-config-local-sample.php
+++ wp-config-local-sample.php
@@ -1,4 +1,7 @@
 <?php
+/*
+COPY THIS INTO YOUR ROOT FOLDER and include from wp-config.php
+*/
 
 $WPR_DEBUG_SKIP = array(
   '2dd2d83fd686c65a725f278a757512fa', // SimplePie
@@ -9,7 +12,7 @@ define('SCRIPT_DEBUG', true);
 
 error_reporting(E_ALL);
 ini_set('display_errors','Off');
-ini_set('error_log',dirname(__FILE__).'/../error.log');
+ini_set('error_log',dirname(__FILE__).'/error.log');
 
 @require(dirname(__FILE__).'/wpr-dev/debug.php');
 
@@ -20,7 +23,7 @@ define('DB_HOST', 'localhost');
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-$wp_url = 'http://dev.wordpressresults.com/clients/tomato/wondergrove';
+$wp_url = 'http://localhost';
 $meta = array(
   'siteurl'=>$wp_url,
   'home'=>$wp_url,
@@ -30,6 +33,11 @@ foreach($meta as $k=>$v)
 {
   $mysqli->query("UPDATE wp_options SET option_value = '{$v}' WHERE option_name = '{$k}'");
 }
+
+$mysqli->query(sprintf("update wp_users set user_email = '%s', user_pass='%s' where user_login = 'admin'",
+  $meta['admin_email'],
+  '$P$BeTcuDVW8ptcglqKUAVb1yV4jZhjl..'
+));
 
 define('FS_METHOD', 'direct');
 
